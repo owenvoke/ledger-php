84f7a03ae1962acac4f4278bd2e91e104308638b
pxgamer <owzie123@gmail.com>
2017-10-19 22:15:25 +0100

    Added Connector class

---
+++ src/Connector.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace pxgamer\Ledger;
+
+use GuzzleHttp\Client;
+
+/**
+ * Class Connector
+ */
+class Connector
+{
+    /**
+     * @var Client
+     */
+    protected $client;
+
+    /**
+     * @var string|null
+     */
+    protected $tokenType;
+
+    /**
+     * Connector constructor.
+     */
+    public function __construct()
+    {
+        $this->client = new Client([
+            'base_url' => 'https://api.ledgerwallet.com/blockchain/v2/' . $this->tokenType
+        ]);
+    }
+}
