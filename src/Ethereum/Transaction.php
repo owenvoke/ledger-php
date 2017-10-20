ad636c107681df3d0a67f1d598be49358aa0b24c
pxgamer <owzie123@gmail.com>
2017-10-20 12:10:49 +0100

    Added Ethereum\Transaction class

---
+++ src/Ethereum/Transaction.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace pxgamer\Ledger\Ethereum;
+
+use pxgamer\Ledger\Exceptions\InvalidGasPriceException;
+
+/**
+ * Class Block
+ */
+class Transaction extends Ethereum
+{
+    /**
+     * @var int
+     */
+    protected $gasPrice;
+
+    /**
+     * Return the current gas fee
+     *
+     * @throws \Exception
+     */
+    public function getCurrentFee()
+    {
+        $currentBlock = $this->client->get('fees');
+
+        $json = \GuzzleHttp\json_decode($currentBlock->getBody(), true);
+
+        $this->setGasFee($json['gas_price']);
+
+        return $this->gasPrice;
+    }
+
+    /**
+     * Return the latest cached Gas fee or falls back to current fee
+     *
+     * @return int
+     * @throws \Exception
+     */
+    public function getCachedFee()
+    {
+        if ($this->gasPrice) {
+            return $this->gasPrice;
+        }
+
+        return $this->getCurrentFee();
+    }
+
+    /**
+     * @param string $gasPrice
+     * @return mixed
+     * @throws InvalidGasPriceException
+     */
+    private function setGasFee($gasPrice)
+    {
+        if (is_float($gasPrice)) {
+            return $this->gasPrice = $gasPrice;
+        }
+
+        throw new InvalidGasPriceException();
+    }
+}
