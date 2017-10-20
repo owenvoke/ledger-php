c92b3db766ae39ffa98e2dcbe7824bf21e08ecae
pxgamer <owzie123@gmail.com>
2017-10-20 11:30:44 +0100

    Added Exceptions\InvalidHeightException

---
+++ src/Exceptions/InvalidHeightException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace pxgamer\Ledger\Exceptions;
+
+/**
+ * Class InvalidHeightException
+ */
+class InvalidHeightException extends \Exception
+{
+    /**
+     * @var string
+     */
+    protected $message = 'Invalid height integer.';
+}
