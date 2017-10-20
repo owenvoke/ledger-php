8b60f9d8d59bef83a18268e738e762dcc37f61c8
pxgamer <owzie123@gmail.com>
2017-10-20 11:35:45 +0100

    Added assertions to EthereumLatestBlockTest

--- tests/EthereumLatestBlockTest.php
+++ tests/EthereumLatestBlockTest.php
@@ -1,11 +1,22 @@
 <?php
 
+use Illuminate\Support\Collection;
+use PHPUnit\Framework\TestCase;
 use pxgamer\Ledger\Ethereum\Block;
 
-class EthereumLatestBlockTest
+class EthereumLatestBlockTest extends TestCase
 {
+    /**
+     * @throws Exception
+     */
     public function testCanGetLatestBlock()
     {
-        $block = Block::getLatestBlock();
+        $block = (new Block)->getLatestBlock();
+
+        $this->assertInstanceOf(Block::class, $block);
+        $this->assertInstanceOf(Collection::class, $block->transactions());
+        $this->assertRegExp('/0x[a-z0-9]{64}/', $block->hash());
+        $this->assertTrue($block->height() > 0);
+        $this->assertInstanceOf(DateTime::class, $block->date());
     }
 }
